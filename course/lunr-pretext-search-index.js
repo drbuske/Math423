404f13d78f8a4e28b31786940f4772af2b4d3d95
Dale Buske <drbuske@stcloudstate.edu>
2026-08-12 14:22:04 +0000

    Latest build deployed.

---
+++ course/lunr-pretext-search-index.js
@@ -0,0 +1,113 @@
+var ptx_lunr_search_style = "textbook";
+var ptx_lunr_docs = [
+{
+  "id": "sec-course-info",
+  "level": "1",
+  "url": "sec-course-info.html",
+  "type": "Section",
+  "number": "",
+  "title": "Course Information",
+  "body": " Course Information  This is the syllabus for Complex Analysis with Applications (MATH 423) for Fall 2026. It is a 3-credit course offered through St. Cloud State University.    Instructor  Dr. Dale Buske, CH 367A, .     Student Hours   Student Support Hours     Day    Times     Mondays  11:00-11:50, 1:00-2:50    Tuesdays  2:00-2:50    Wednesdays  9:00-10:50    Thursdays  1:00-2:50    Fridays  11:00-11:50        Accessibility Services  Please contact Student Accessibility Services (SAS), or 320-308-4080, Centennial Hall 202, to meet and discuss reasonable and appropriate accommodations.    Class Meetings  Attendance and active participation in class is strongly correlated to academic success. You will find attending class to be quite important, useful, and (if all goes well) fun!     Course Description  Algebra and geometry of the complex number field, limits, continuity, analytic and harmonic functions, elementary conformal mappings, series, integrals, residues, and applications to physics and engineering. 3 cr    Prerequisite  Math 320 (Multivariable Calculus for Engineers) or Math 321 (Vector and Multivariable Calculus)    Academic Integrity  There is nothing more important in this class than academic integrity . Sure, you want to master the topics in the course (and perhaps earn a good grade). Sure, you want to enjoy doing it. And, it is also important to know that your instructor intends to play his part in helping you be successful. Succeeding, as you know, will require hard work and significant effort. Cutting corners will not serve you well.  University policy is clear: Work submitted by an individual as her\/his\/their own course work in connection with an application for a position, promotion, tenure, salary increase, or other academic activity must be academically honest and a product of his or her own efforts. (A.II, 2.4, SUB.d.2, Professional and Ethical Standards for Employees and Students).    Cheating cannot be tolerated. All work must be your own. Suspected incidents of academic dishonesty will be aggressively managed via the procedure found in the SCSU student handbook .    In this course, students are likely to use AI tools to enhance their learning experience. However, it is essential to use these tools ethically and responsibly. AI can be a valuable resource for understanding complex concepts and solving problems, but it should not replace your own critical thinking and problem-solving efforts. All submitted work must be your own, and any assistance from AI tools must be properly acknowledged on written work. The use of AI is forbidden on assessments of your knowledge (e.g. exams). Using AI to complete work without acknowledgment will be considered a violation of academic integrity policies. In mathematics, AI often gives incorrect responses (i.e. hallucinations) and large language models such as ChatGPT are widely deemed “unreliable” in doing mathematics. Relaying AI responses that are incorrect will earn no credit. However, not acknowledging the use of AI in a response is far worse – it is academic misconduct.    Course Materials  The course textbook is Complex Variables and Applications by James Ward Brown and Ruel V. Churchill, McGrow Hill, 8th edition, 2009. ISBN: 9780073051949.  Course materials beyond the textbook and course packet can be found at https:\/\/drbuske.github.io\/Math423\/course\/ . There you will find items that include the course syllabus.   D2L Brightspace will be the learning management tool used to organize the course. Grades, a calendar, and assignments as well as announcements will be posted on D2L. Visit and login to your account using your Huskynet ID and password.       Tips for Success  Engage your peers. Engage your instructor. Your success is in their best interest too! Mathematics is a discipline that benefits greatly from working together in gaining a deep understanding.  Practice. Do math. Watching others do math is a passive activity with far less value than attempting to solve problems. We learn from our mistakes and struggles. If it were easy, everyone would do it and it would have less value. Remember the rule of thumb – expect to study 2-3 hours outside of class for every hour in class. That means 8-12 hours per week of study out of class!  Assignments have been structured to maximize your learning experience. This effort is as critical as any in your success. 95% of what you learn will be outside the classroom. Faithfully attempting the assignments is a significant piece of this learning.   "
+},
+{
+  "id": "sec-course-info-2",
+  "level": "2",
+  "url": "sec-course-info.html#sec-course-info-2",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "Complex Analysis with Applications (MATH 423) textbook D2L Brightspace "
+},
+{
+  "id": "syllabus-3",
+  "level": "1",
+  "url": "syllabus-3.html",
+  "type": "Section",
+  "number": "",
+  "title": "Topics and Learning Outcomes",
+  "body": " Topics and Learning Outcomes     Topics   Complex numbers: Sums and Products. Basic Algebraic Properties. Moduli. Complex Conjugates. Exponential Form. Roots of Complex Numbers. Regions in the Complex Plane.   Analytic Functions: Functions of a Complex Variable. Mappings. Limits. Theorems on Limits. Limits Involving the Point at Infinity. Continuity. Derivatives. Differentiation Formulas. Cauchy-Riemann Equations. Sufficient Conditions for Differentiability. Polar Coordinates. Analytic Functions. Harmonic Functions.   Elementary Functions: The Exponential Function. The Logarithmic Function. Branches and Derivatives of Logarithms. Complex Exponents. Trigonometric and Hyperbolic Functions. Inverse Trigonometric and Hyperbolic Functions.   Integrals: Derivatives of Functions . Definite Integrals of Functions . Contours and Contour Integrals. Antiderivatives. Cauchy-Goursat Theorem. Simply and Multiply Connected Domains. Cauchy Integral Formula. Derivatives of Analytic Functions. Liouville’s Theorem and the Fundamental Theorem of Algebra.   Series: Convergence of Sequences and Series. Taylor and Laurent Series. Absolute and Uniform Convergence of Power Series. Continuity of Sums of Power Series. Integration and Differentiation of Power Series. Uniqueness of Series Representations.   Residues and Poles: Residues and Cauchy’s Residue Theorem. The Three Types of Isolated Singular Points. Residues at Poles. Zeros of Analytic Functions. Zeros and Poles. Behavior of Functions Near Isolated Singular Points. Evaluation of Improper Integrals.    Learning Outcomes    Perform basic algebraic operations using various representations of the complex number system.  Prove simple facts about the topology of the complex number system and functions of a single complex variable.  Compare and contrast functions of a real variable and functions of a complex variable.  Identify and differentiate analytic functions.  Find harmonic functions that solve classical applied problems in mathematics.  Define complex generalizations of the definite integral of a function of a real variable.  Compute contour integrals.  State the Cauchy Integral Formula and Liouville’s Theorem.  Compute the Taylor and Laurent expansions of simple functions, determining the nature of the singularities, regions of convergence, and calculating residues  State and apply named theorems of calculus (the Intermediate Value Theorem, the Mean Value Theorem, and the Fundamental Theorem of Calculus).  Use the Cauchy Residue Theorem to evaluate integrals.       "
+},
+{
+  "id": "subsec-calendar",
+  "level": "1",
+  "url": "subsec-calendar.html",
+  "type": "Section",
+  "number": "",
+  "title": "Calendar",
+  "body": " Calendar  Below is an estimate for when we will discuss various topics in the text and when exams are scheduled.   Course Schedule    Week Starting  Tuesday   Thursday    8\/24  1-2   3-4    8\/31  5-7   8-10    9\/7  11-13   14-16    9\/14  17-19   20-21    9\/21  22-25   26-28    9\/28  Exam 1   29-31    10\/5  32-35   36-37    10\/12  38-39   40-41    10\/19  42-43   44-46    10\/26  47-48   49-50    11\/2  51-52   Exam 2    11\/9  53-54   55-58    11\/16  59-62   63-66    11\/23  68-70   No Class    11\/30  71-72   73-75     Exams will cover the following material from the course.   Exam dates and topics     Exam    Date    Topics     Exam 1  September 29  Sections 1-28    Exam 2  November 5  Sections 29-52    Final Exam  See SCSU final exam schedule   Sections 1-75     To be successful, sufficient time will need to be dedicated to your studies. Studying on weekends to get ahead, especially if holding a job or other commitments during the work week, will be helpful. Below is a table with time you might expect to spend on each aspect of the course (for each day of class).   Estimated Time on Various Aspects of the Course     Item    Estimated Time Per Section     Reading Textbook  60-90 minutes    Written Homework  60-120 minutes    Reviewing Notes  15 minutes     "
+},
+{
+  "id": "schedule-table",
+  "level": "2",
+  "url": "subsec-calendar.html#schedule-table",
+  "type": "Table",
+  "number": "2",
+  "title": "Course Schedule",
+  "body": " Course Schedule    Week Starting  Tuesday   Thursday    8\/24  1-2   3-4    8\/31  5-7   8-10    9\/7  11-13   14-16    9\/14  17-19   20-21    9\/21  22-25   26-28    9\/28  Exam 1   29-31    10\/5  32-35   36-37    10\/12  38-39   40-41    10\/19  42-43   44-46    10\/26  47-48   49-50    11\/2  51-52   Exam 2    11\/9  53-54   55-58    11\/16  59-62   63-66    11\/23  68-70   No Class    11\/30  71-72   73-75    "
+},
+{
+  "id": "ExamDates",
+  "level": "2",
+  "url": "subsec-calendar.html#ExamDates",
+  "type": "Table",
+  "number": "3",
+  "title": "Exam dates and topics",
+  "body": " Exam dates and topics     Exam    Date    Topics     Exam 1  September 29  Sections 1-28    Exam 2  November 5  Sections 29-52    Final Exam  See SCSU final exam schedule   Sections 1-75    "
+},
+{
+  "id": "StudyTime",
+  "level": "2",
+  "url": "subsec-calendar.html#StudyTime",
+  "type": "Table",
+  "number": "4",
+  "title": "Estimated Time on Various Aspects of the Course",
+  "body": " Estimated Time on Various Aspects of the Course     Item    Estimated Time Per Section     Reading Textbook  60-90 minutes    Written Homework  60-120 minutes    Reviewing Notes  15 minutes    "
+},
+{
+  "id": "sec-assessments",
+  "level": "1",
+  "url": "sec-assessments.html",
+  "type": "Section",
+  "number": "",
+  "title": "Assessments and Grades",
+  "body": " Assessments and Grades     Written Homework  Each Thursday, hand-written solutions to problems from the textbook will be due. Additional problems that are not from the textbook may also be assigned from time-to-time. You must document names of other people (e.g. students, faculty) and AI systems that you consult. Overconsultation with AI systems will likely be obvious, potentially non-productive, and potentially academically dishonest. Remember that your goal in this class is to learn mathematics and the honest struggle is often the best way to navigate towards this goal.   Written Homework Assignments    Due Date  Questions     8\/27  Section 2: #1(a), 2(b), 9, 10     9\/3  Section 3: #2; Section 4: #4, 5; Section 5: #2, 7, 9, 11, 12     9\/10  Section 8: #1(b), 3, 4, 5(c); Section 10: #3(a), 8(b); Section 11: #1(a)(c)(e)(f), 2, 6, 7(a)(b); Section 12: 1(a)(d), 3, 4     9\/17  Section 14: #2, 3, 7; Section 18: #1(b)(c), 2(a)(b), 5, 10(a)(b)     9\/24  Section 20: #4, 6, 7, 8; Section 23: #1(b), 2(c), 3(c), 4(b), 8     10\/1  Section 25: #1(b), 2(c), 7; Section 26: #1(d), 2, 3, 5     10\/8  Section 29: #3, 5, 7, 8(b), 10, 13; Section 31: #1(a), 4(b), 5(b); Section 32: #1, 3; Section 33: #2(c), 3, 9; Section 34: #2, 9, 11, 13     10\/15  Section 35: #3, 6, 15(b); Section 36: #1(b), 5; Section 38: #1(b), 3, 4, 5; Section 39: #1(a), 5     10\/22  Section 42: #1, 2, 4, 6, 7, 10; Section 43: #1, 2, 4     10\/29  Section 45: #3, 5     11\/5  Section 49: #1(a)(f), 3, 6; Section 52: #1(a)(b), 2(a), 3, 7     11\/12  Section 54: #1, 3, 4, 5     11\/19  Section 56: #3, 4, 6; Section 59: #1, 3, 6, 7; Section 62: 1, 3, 5, 6, 7     11\/26  Nothing Due     12\/3  Section 66: #3, 4, 6, 11        Exams  There will be two (2) in-class written, proctored exams, and one (1) final written, proctored exam. Each exam may have a take-home component. All exams will be independent assessments of your knowledge.  The oral exams will be 15-20 minutes in length. The first will take place just prior to midterm and the second will take place during the last two weeks of the term. The concept may seem intimidating at first, but past student response suggest this to be a valuable means of assessment and academic growth.   Oral Exam Rubric     Points    Description     1  Unsatisfactory execution and\/or communication with fundamental errors.    2  Flawed execution with nontrivial errors and poor communication.    3  Adequately executed but with some nontrivial errors and inconsistent communication.    4  Generally well-executed but may have minor communication flaws and some math errors.    5  Well-executed, well-communicated, essentially correct.       Grading  I will use my best professional judgment in determining grades. While many courses utilize 90%, 80%, 70%, and 60% as cutoffs for A, B, C, and D grades, these will serve only as guidelines. Averages above these marks will guarantee these grades but lower marks may earn higher final grades.   Grade weighting     Item    Weighting     Written Homework)  27%    Written Exams (2)  36%    Oral Exams (2)  10%    Final Exam  27%        "
+},
+{
+  "id": "schedule-table-homework",
+  "level": "2",
+  "url": "sec-assessments.html#schedule-table-homework",
+  "type": "Table",
+  "number": "5",
+  "title": "Written Homework Assignments",
+  "body": " Written Homework Assignments    Due Date  Questions     8\/27  Section 2: #1(a), 2(b), 9, 10     9\/3  Section 3: #2; Section 4: #4, 5; Section 5: #2, 7, 9, 11, 12     9\/10  Section 8: #1(b), 3, 4, 5(c); Section 10: #3(a), 8(b); Section 11: #1(a)(c)(e)(f), 2, 6, 7(a)(b); Section 12: 1(a)(d), 3, 4     9\/17  Section 14: #2, 3, 7; Section 18: #1(b)(c), 2(a)(b), 5, 10(a)(b)     9\/24  Section 20: #4, 6, 7, 8; Section 23: #1(b), 2(c), 3(c), 4(b), 8     10\/1  Section 25: #1(b), 2(c), 7; Section 26: #1(d), 2, 3, 5     10\/8  Section 29: #3, 5, 7, 8(b), 10, 13; Section 31: #1(a), 4(b), 5(b); Section 32: #1, 3; Section 33: #2(c), 3, 9; Section 34: #2, 9, 11, 13     10\/15  Section 35: #3, 6, 15(b); Section 36: #1(b), 5; Section 38: #1(b), 3, 4, 5; Section 39: #1(a), 5     10\/22  Section 42: #1, 2, 4, 6, 7, 10; Section 43: #1, 2, 4     10\/29  Section 45: #3, 5     11\/5  Section 49: #1(a)(f), 3, 6; Section 52: #1(a)(b), 2(a), 3, 7     11\/12  Section 54: #1, 3, 4, 5     11\/19  Section 56: #3, 4, 6; Section 59: #1, 3, 6, 7; Section 62: 1, 3, 5, 6, 7     11\/26  Nothing Due     12\/3  Section 66: #3, 4, 6, 11     "
+},
+{
+  "id": "OralExamRubric",
+  "level": "2",
+  "url": "sec-assessments.html#OralExamRubric",
+  "type": "Table",
+  "number": "6",
+  "title": "Oral Exam Rubric",
+  "body": " Oral Exam Rubric     Points    Description     1  Unsatisfactory execution and\/or communication with fundamental errors.    2  Flawed execution with nontrivial errors and poor communication.    3  Adequately executed but with some nontrivial errors and inconsistent communication.    4  Generally well-executed but may have minor communication flaws and some math errors.    5  Well-executed, well-communicated, essentially correct.    "
+},
+{
+  "id": "GradeWeights",
+  "level": "2",
+  "url": "sec-assessments.html#GradeWeights",
+  "type": "Table",
+  "number": "7",
+  "title": "Grade weighting",
+  "body": " Grade weighting     Item    Weighting     Written Homework)  27%    Written Exams (2)  36%    Oral Exams (2)  10%    Final Exam  27%    "
+}
+]
+
+var ptx_lunr_idx = lunr(function () {
+  this.ref('id')
+  this.field('title')
+  this.field('body')
+  this.metadataWhitelist = ['position']
+
+  ptx_lunr_docs.forEach(function (doc) {
+    this.add(doc)
+  }, this)
+})
